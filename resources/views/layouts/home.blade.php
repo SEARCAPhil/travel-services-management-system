11bcf96a7416e2f7c00f72819412891a215b3e76
kenneth <johnkennethgibasabella@gmail.com>
2016-11-14 16:40:49 +0800

    inital laravel installation

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title')</title>
+
+</head>
+<body>
+<link rel="stylesheet" href="{{asset('css/custom.css')}}">
+@section('content')
+	@show
+
+</body>
+</html>
---
+++ resources/views/page.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.home')
+@section('title','test')
+@section('content')
+	@parent
+		<p>Home Page</p>
+@endsection	
