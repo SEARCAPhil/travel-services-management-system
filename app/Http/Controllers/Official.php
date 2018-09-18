1854289f2fd23c7551ba245f80cac60845f1061c
jkga <johnkennethgibasabella@gmail.com>
2018-09-18 15:19:11 +0800

    Removed dumping

--- app/Http/Controllers/Official.php
+++ app/Http/Controllers/Official.php
@@ -436,7 +436,6 @@ class Official extends Controller
             $official_signatory = new Directory();
             $signatory = json_decode($official_signatory->signatory_department($_SESSION['dept_id']));
 
-var_dump($signatory); exit;
             $approved_by=@$signatory[0]->profile_name;
             $approved_by_id=NULL;
             $recommended_by_id=NULL;
